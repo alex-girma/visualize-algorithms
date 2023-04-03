08e33ecc73e7b7c4ff31817792059e7b3ce07eb0
Alemayehu Girma <alemayehu.girma@hotmail.com>
2023-04-03 21:11:41 +0200

    sort

--- components/sort/SortVisualizer.tsx
+++ components/sort/SortVisualizer.tsx
@@ -1,9 +1,11 @@
 const SortVisualizer = ({ number }: { number: number }) => {
   return (
     <div
-      className="rotatex0 duration-750 flex w-8 items-end justify-center bg-gray-400 text-gray-900"
+      className="rotatex0 flex w-8 items-end justify-center bg-gray-400 text-gray-900 "
       id={String(number)}
-      style={{ height: `${number * 4 + 20}px` }}
+      style={{
+        height: `${number * 4 + 20}px`,
+      }}
     >
       {number}
     </div>
